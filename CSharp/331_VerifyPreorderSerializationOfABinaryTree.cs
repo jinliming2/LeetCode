fa7bfaed4f0b1ef4cdd869bafd027f93787ce647
Liming Jin <jinliming2@gmail.com>
2016-09-05 14:01:21 +0800

    👮#331

---
+++ CSharp/331_VerifyPreorderSerializationOfABinaryTree.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace _331_VerifyPreorderSerializationOfABinaryTree {
+    public class Solution {
+        private struct node {
+            public string character;
+            public bool left;
+            public bool right;
+        }
+        public bool IsValidSerialization(string preorder) {
+            if(preorder == "#") {
+                return true;
+            }
+            bool root = false;
+            string[] list = preorder.Split(',');
+            Stack<node> stack = new Stack<node>();
+            foreach(string str in list) {
+                if(stack.Count == 0) {
+                    if(root) {
+                        return false;
+                    } else {
+                        root = true;
+                    }
+                }
+                while(stack.Count > 0) {
+                    node lasts = stack.Pop();
+                    if(!lasts.left || !lasts.right) {
+                        stack.Push(lasts);
+                        break;
+                    }
+                }
+                if(str == "#") {
+                    if(stack.Count <= 0) {
+                        return false;
+                    }
+                    node last = stack.Pop();
+                    if(!last.left) {
+                        last.left = true;
+                        stack.Push(last);
+                    } else if(last.right) {
+                        return false;
+                    }
+                } else {
+                    if(stack.Count > 0) {
+                        node last = stack.Pop();
+                        if(!last.left) {
+                            last.left = true;
+                        } else {
+                            last.right = true;
+                        }
+                        stack.Push(last);
+                    }
+                    node current;
+                    current.character = str;
+                    current.left = false;
+                    current.right = false;
+                    stack.Push(current);
+                }
+            }
+            while(stack.Count > 0) {
+                node lasts = stack.Pop();
+                if(!lasts.left || !lasts.right) {
+                    stack.Push(lasts);
+                    break;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ CSharpUnitTest/331_VerifyPreorderSerializationOfABinaryTreeTest.cs
@@ -0,0 +1,21 @@
+﻿using _331_VerifyPreorderSerializationOfABinaryTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CSharpUnitTest {
+    [TestClass]
+    public class _331_VerifyPreorderSerializationOfABinaryTreeTest {
+        Solution solution;
+        public _331_VerifyPreorderSerializationOfABinaryTreeTest() {
+            solution = new Solution();
+        }
+        [TestMethod]
+        public void IsValidSerializationTest() {
+            Assert.AreEqual(true, solution.IsValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
+            Assert.AreEqual(false, solution.IsValidSerialization("1,#"));
+            Assert.AreEqual(false, solution.IsValidSerialization("9,#,#,1"));
+            Assert.AreEqual(true, solution.IsValidSerialization("#"));
+            Assert.AreEqual(false, solution.IsValidSerialization("9,3,4,#,#,1,#,#,#,2,#,6,#,#"));
+        }
+    }
+}
