07ce723f99103486abe05dc66b0c8f8622d1f82f
Liming Jin <jinliming2@gmail.com>
2020-12-31 10:00:37 +0800

    Add a test case for #111 🐧

--- Go/111_Minimum_Depth_of_Binary_Tree/main_test.go
+++ Go/111_Minimum_Depth_of_Binary_Tree/main_test.go
@@ -88,6 +88,36 @@ var testCase = map[*TreeNode]int{
 			},
 		},
 	}: 4,
+	{
+		Val: 0,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 5,
+				},
+				Right: &TreeNode{
+					Val: 1,
+				},
+			},
+		},
+		Right: &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val: 3,
+				Right: &TreeNode{
+					Val: 6,
+				},
+			},
+			Right: &TreeNode{
+				Val: -1,
+				Right: &TreeNode{
+					Val: 8,
+				},
+			},
+		},
+	}: 4,
 }
 
 func TestMinDepthBFS(t *testing.T) {
